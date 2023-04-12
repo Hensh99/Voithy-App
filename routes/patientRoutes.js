d9c907071b4465ee8c5c7e7c1479eb72bd56cda4
Mohamed Hany <mohamedhany.mh.1999@gmail.com>
2023-04-12 21:00:11 +0200

    Routes

---
+++ routes/patientRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const patientController = require("./../controllers/patientController");
+const router = express.Router();
+
+router
+  .route("/")
+  .get(patientController.getAllPatients)
+  .post(patientController.createPatient);
+
+router
+  .route("/:id")
+  .get(patientController.getPatient)
+  .patch(patientController.updatePatient)
+  .delete(patientController.deletePatient);
+
+module.exports = router;
