af191761c7941c28536ca54f17155b398f650ac3
Mohamed Hany <mohamedhany.mh.1999@gmail.com>
2023-04-27 13:38:52 +0200

    Catching Errors in Async Functions

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,135 +1,94 @@
 const Patient = require("./../models/patientModel");
 const APIFeatures = require("./../utils/apiFeatures");
+const catchAsync = require("./../utils/catchAsync");
 
-exports.getAllPatients = async (req, res) => {
-  try {
-    // EXECUTE QUERY
-    const features = new APIFeatures(Patient.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const patients = await features.query;
+exports.getAllPatients = catchAsync(async (req, res, next) => {
+  // EXECUTE QUERY
+  const features = new APIFeatures(Patient.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const patients = await features.query;
 
-    // RESPONSE
-    res.status(200).json({
-      status: "success",
-      results: patients.length,
-      data: {
-        patients,
+  // RESPONSE
+  res.status(200).json({
+    status: "success",
+    results: patients.length,
+    data: {
+      patients,
+    },
+  });
+});
+
+exports.getPatient = catchAsync(async (req, res, next) => {
+  const patient = await Patient.findById(req.params.id);
+  // Patient.findOne({_id: req.params.id})
+  res.status(200).json({
+    status: "success",
+    data: {
+      patient,
+    },
+  });
+});
+
+exports.createPatient = catchAsync(async (req, res, next) => {
+  const newPatient = await Patient.create(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      patient: newPatient,
+    },
+  });
+});
+
+exports.updatePatient = catchAsync(async (req, res, next) => {
+  const patient = await Patient.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      patient,
+    },
+  });
+});
+
+exports.deletePatient = catchAsync(async (req, res, next) => {
+  await Patient.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
+exports.getPatientStats = catchAsync(async (req, res, next) => {
+  const stats = await Patient.aggregate([
+    {
+      $match: { age: { $gte: 15 } },
+    },
+    {
+      $group: {
+        _id: { $toUpper: "$age" },
+        numPatients: { $sum: 1 },
+        avgAge: { $avg: "$age" },
+        avgHeight: { $avg: "$height" },
+        avgWeight: { $avg: "$weight" },
+        minAge: { $min: "$age" },
+        maxAge: { $max: "$age" },
       },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-};
-
-exports.getPatient = async (req, res) => {
-  try {
-    const patient = await Patient.findById(req.params.id);
-    // Patient.findOne({_id: req.params.id})
-    res.status(200).json({
-      status: "success",
-      data: {
-        patient,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-};
-
-exports.createPatient = async (req, res) => {
-  try {
-    const newPatient = await Patient.create(req.body);
-
-    res.status(201).json({
-      status: "success",
-      data: {
-        patient: newPatient,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-};
-
-exports.updatePatient = async (req, res) => {
-  try {
-    const patient = await Patient.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    res.status(200).json({
-      status: "success",
-      data: {
-        patient,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-};
-
-exports.deletePatient = async (req, res) => {
-  try {
-    await Patient.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: "success",
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-};
-
-exports.getPatientStats = async (req, res) => {
-  try {
-    const stats = await Patient.aggregate([
-      {
-        $match: { age: { $gte: 15 } },
-      },
-      {
-        $group: {
-          _id: { $toUpper: "$age" },
-          numPatients: { $sum: 1 },
-          avgAge: { $avg: "$age" },
-          avgHeight: { $avg: "$height" },
-          avgWeight: { $avg: "$weight" },
-          minAge: { $min: "$age" },
-          maxAge: { $max: "$age" },
-        },
-      },
-      {
-        $sort: { minAge: 1 },
-      },
-    ]);
-    res.status(200).json({
-      status: "success",
-      data: {
-        stats,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-};
+    },
+    {
+      $sort: { minAge: 1 },
+    },
+  ]);
+  res.status(200).json({
+    status: "success",
+    data: {
+      stats,
+    },
+  });
+});
