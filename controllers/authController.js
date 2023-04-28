36d2f43b298aa127553449b0617672ab83191619
Mohamed Hany <mohamedhany.mh.1999@gmail.com>
2023-04-28 18:38:28 +0200

    Restricted Input User Data [Avoid Security Flaw]

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,12 @@ const user = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await user.create(req.body);
+  const newUser = await user.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: "Success",
