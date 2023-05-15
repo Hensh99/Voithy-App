257af55ea9ceee41d309a6f62b0a7b3bb463612d
Mohamed Hany <mohamedhany.mh.1999@gmail.com>
2023-05-15 21:10:04 +0200

    Add the protect middlware function to getAllUsers

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const patientController = require("./../controllers/patientController");
+const authController = require("./../controllers/authController");
+
 const router = express.Router();
 
 router.route("/patient-stats").get(patientController.getPatientStats);
 
 router
   .route("/")
-  .get(patientController.getAllPatients)
+  .get(authController.protect, patientController.getAllPatients)
   .post(patientController.createPatient);
 
 router
