c50a1bfe4ede987e25c6785df857bad05f3c3f29
Mohamed Hany <mohamedhany.mh.1999@gmail.com>
2023-05-15 23:48:24 +0200

    Verify the token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
@@ -61,10 +62,13 @@ exports.protect = catchAsync(async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(" ")[1];
   }
-  console.log(token);
-
   if (!token) {
     return next(new AppError("Please login to get access.", 401));
   }
+
+  // 2) Verify the token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) Check if the user is still exists
   next();
 });
