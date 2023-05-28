3e4f1b9bceff950ec81d70aef54daba5c684f133
Mohamed Hany <mohamedhany.mh.1999@gmail.com>
2023-05-28 13:24:28 +0200

    Global middleware to limit the number of request

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,11 +9,20 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// 1) MIDDLEWARE
+// 1) GLOBAL MIDDLEWARE
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// To avoid Brute Force attack or DNS
+const limiter = rateLimit({
+  max: 100, // 100 request for the same IP
+  windowMs: 60 * 60 * 1000, // 1 Hour
+  message:
+    "Alert, Too many request from the same IP Address, try again after an hour.",
+});
+app.use("/api", limiter);
 
 app.use(express.json()); // middleware :function to modify the incoming request data
 
